c8739e19801d0374f4581bcca14286c2d92958fd
S.Yoshida <fromage.blanc.music@gmail.com>
2015-09-01 11:08:34 +0900

    Update const.php

--- const.php
+++ const.php
@@ -29,18 +29,18 @@
   // twitter
   define('TWITTER_CONSUMER_KEY','Yi7VFYg7s44***VBdFA');
   define('TWITTER_CONSUMER_SECRET','F***IVHLqPw***NPTkVxnPXoBoTO9SfK***WugA7Q');
-  define('TWITTER_CALLBACK','http://software-instrument.com/callback.php');
+  define('TWITTER_CALLBACK','http://******-instrument.com/callback.php');
   define('TWITTER_ACCESS_TOKEN','1177***122-YYO00t***6ZT8aNzpBDiJK4s9LUSyI***v2Og3f');
   define('TWITTER_ACCESS_TOKEN_SECRET','dsy6FwpctX***m85J1A***a9u5rqPCs***zrbKYArA');
   
   // yahoo
   define('YAHOO_APP_KEY','dj0zaiZpPVJFcndHTWQ2djdHVSZ***lXazli***YzJwMk5tc21jR285TUEtLSZzPWNvbnN1bWVyc2VjcmV***g9Y2U-');
   define('YAHOO_APP_SECRET','9e5da97f87adb118***305dc***959c0b7c102ac');
-  define('YAHOO_CALLBACK','http://software-instrument.com/ycallback.php');
+  define('YAHOO_CALLBACK','http://*********-********.com/ycallback.php');
 
   // google
   define('GOOGLE_CLIENT_ID', '86987***0521.apps.googleusercontent.com');
   define('GOOGLE_CLIENT_SECRET', '8***P5GnXi2f***_SZQ5X3Ze');
-  define('GOOGLE_CALLBACK', 'http://software-instrument.com/oauth2callback.php');
+  define('GOOGLE_CALLBACK', 'http://******-************.com/oauth2callback.php');
   
 ?>
